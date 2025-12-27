03b1e66342ff9d602839d8fe2f2e128f1b0d3cb0
Lucas Falcao Lopes <lfalcaolopes@gmail.com>
2025-12-27 00:02:54 -0300

    chore: setup shadcn and jest

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
+}
